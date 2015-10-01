54305270e3ded2778ea8b76de6c24bfcaf588d25
Iñigo Molinuevo <i.m.parron@opendeusto.es>
2015-10-01 19:40:43 +0200

    Card and Deck. Iñigo Molinuevo

--- src/main/java/Card.java
+++ src/main/java/Card.java
@@ -1,3 +1,20 @@
 public class Card {
 
+	private int value;
+
+	private SuitValue suitValue;
+
+	public Card(int value, SuitValue suitValue) {
+		this.value = value;
+		this.suitValue = suitValue;
+	}
+
+	public int getValue() {
+		return value;
+	}
+
+	public SuitValue getSuitValue() {
+		return suitValue;
+	}
+
 }
--- src/main/java/Deck.java
+++ src/main/java/Deck.java
@@ -1,3 +1,28 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Deck {
+
+	private ArrayList<Card> cards;
+
+	public Deck() {
+		createCardsOfSuit(cards, SuitValue.HEARTS);
+		createCardsOfSuit(cards, SuitValue.TILES);
+		createCardsOfSuit(cards, SuitValue.CLOVERS);
+		createCardsOfSuit(cards, SuitValue.PIKES);
+		Collections.shuffle(cards);
+	}
+
+	private void createCardsOfSuit(ArrayList<Card> cards, SuitValue suitValue) {
+		for (int i = 0; i < 13; i++) {
+			cards.add(new Card(i + 1, suitValue));
+		}
+	}
+	
+	public Card getCardFromTop() {
+		Card card = cards.get(0);
+		cards.remove(0);
+		return card;
+	}
 
 }
