727a4303087b0d3f152b287c6b8d0b3ceeb9ce2a
Iñigo Molinuevo <i.m.parron@opendeusto.es>
2015-10-02 17:14:43 +0200

    MenuView edit. Iñigo Molinuevo

--- src/main/java/MenuView.java
+++ src/main/java/MenuView.java
@@ -1,12 +1,29 @@
+import java.util.Scanner;
+
 public class MenuView {
 
+	private MenuController menuController;
+
 	public MenuView(MenuController menuController) {
-		// TODO Auto-generated constructor stub
+		this.menuController = menuController;
 	}
 
 	public void render() {
-		// TODO Auto-generated method stub
-		
+		System.out.println("--------------------------");
+		System.out.println("1. Move from deck to draw");
+		System.out.println("2. Move from draw to suit");
+		System.out.println("3. Move from draw to rank");
+		System.out.println("4. Move from rank to suit");
+		System.out.println("5. Move from rank to rank");
+		System.out.println("6. Move from suit to rank");
+		int menuOption = 0;
+		do {
+			System.out.print("Option? [1-6]: ");
+			@SuppressWarnings("resource")
+			Scanner in = new Scanner(System.in);
+			menuOption = in.nextInt();
+		} while ((menuOption > 6) && (menuOption < 1));
+		menuController.control(menuOption);
 	}
 
 }
