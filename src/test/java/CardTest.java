40c3fd5a800ef4c37d1fa233d76a55671b6998d2
Iñigo Molinuevo <i.m.parron@opendeusto.es>
2015-10-04 11:08:52 +0200

    Card and Rank test implemented. Iñigo Molinuevo

--- src/test/java/CardTest.java
+++ src/test/java/CardTest.java
@@ -1,12 +1,27 @@
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class CardTest {
 
+	private Card card;
+
+	@Before
+	public void before() {
+		card = new Card(2, SuitValue.CLOVERS);
+
+	}
+
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testCardIntSuitValue() {
+		assertEquals(2, card.getValue());
+		assertEquals(SuitValue.CLOVERS, card.getSuitValue());
+	}
+
+	@Test
+	public void testGetColor() {
+		assertEquals('b', card.getColor());
 	}
 
 }
--- src/test/java/RankTest.java
+++ src/test/java/RankTest.java
@@ -1,12 +1,22 @@
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class RankTest {
 
+	private Rank rank;
+
+	@Before
+	public void before() {
+		rank = new Rank();
+	}
+
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testAddCard() {
+		assertEquals(0, rank.size());
+		rank.addCard(new Card(2, SuitValue.CLOVERS));
+		assertEquals(1, rank.size());
 	}
 
 }
