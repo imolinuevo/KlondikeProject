67e1ab42a42994dbb6aab342fcdeee40b77ad042
Iñigo Molinuevo <i.m.parron@opendeusto.es>
2015-10-04 10:36:32 +0200

    More tests added. Iñigo Molinuevo

--- src/main/java/SuitValue.java
+++ src/main/java/SuitValue.java
@@ -1,16 +1,13 @@
 public enum SuitValue {
 
-	HEARTS('r'),
-	TILES('r'),
-	CLOVERS('b'),
-	PIKES('b');
-	
+	HEARTS('r'), TILES('r'), CLOVERS('b'), PIKES('b');
+
 	private char value;
-	
+
 	private SuitValue(char value) {
 		this.value = value;
 	}
-	
+
 	public char getValue() {
 		return this.value;
 	}
---
+++ src/test/java/CardGroupTest.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CardGroupTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
